5e370fa34f0265d2a1eede0d41392c89467a6f1e
KristianSp27 <101999488+KristianSp27@users.noreply.github.com>
2022-07-19 00:09:18 +0300

    Add files via upload

---
+++ passwordGuess.js
@@ -0,0 +1,8 @@
+function passwordGuess(input) {
+    let password = String(input[0]);
+    if (password == "s3cr3t!P@ssw0rd") {
+        console.log("Welcome")
+    } else {
+        console.log("Wrong password!");
+    }
+}
