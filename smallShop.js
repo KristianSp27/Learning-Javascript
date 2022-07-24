09046572ce0b40d5e14fc1a2a2317d801367c771
KristianSp27 <101999488+KristianSp27@users.noreply.github.com>
2022-07-24 23:39:16 +0300

    Add files via upload

---
+++ smallShop.js
@@ -0,0 +1,46 @@
+function smallShop(input) {
+    let product = input[0];
+    let town = input[1];
+    let quantity = Number(input[2]);
+    let sum = 0;
+    if (town === "Sofia" ) {
+        if (product === "coffee") {
+            sum = quantity * 0.50;
+        } else if (product === "water") {
+            sum = quantity * 0.80;
+        } else if (product === "beer") {
+            sum = quantity * 1.20;
+        } else if (product === "sweets") {
+            sum = quantity * 1.45;
+        } else {
+            sum = quantity * 1.60;
+        }
+    }
+    if (town === "Plovdiv" ) {
+        if (product === "coffee") {
+            sum = quantity * 0.40;
+        } else if (product === "water") {
+            sum = quantity * 0.70;
+        } else if (product === "beer") {
+            sum = quantity * 1.15;
+        } else if (product === "sweets") {
+            sum = quantity * 1.30;
+        } else {
+            sum = quantity * 1.50;
+        }
+    }
+    if (town === "Varna" ) {
+        if (product === "coffee") {
+            sum = quantity * 0.45;
+        } else if (product === "water") {
+            sum = quantity * 0.70;
+        } else if (product === "beer") {
+            sum = quantity * 1.10;
+        } else if (product === "sweets") {
+            sum = quantity * 1.35;
+        } else {
+            sum = quantity * 1.55;
+        }
+    }
+    console.log(sum);
+}
