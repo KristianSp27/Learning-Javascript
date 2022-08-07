e5d3f8d6f83f2bb07c9c78a3b1ce814d22e15f74
KristianSp27 <101999488+KristianSp27@users.noreply.github.com>
2022-08-07 20:32:35 +0300

    Add files via upload

--- multiplicationTable.js
+++ multiplicationTable.js
@@ -1,9 +1,8 @@
-function multiplicationTable(input) {
-    let num = Number(input[0]);
-    
-    for (let i = 1; i <= 10; i++) {
-        let res = i * num;
-        console.log(`${i} * ${num} = ${res}`);
+function multiplicationTable() {
+  for (a = 1; a <= 10; a++) {
+    for (b = 1; b <= 10; b++) {
+      let result = a * b;
+      console.log(`${a} * ${b} = ${result}`);
     }
-    
+  }
 }
